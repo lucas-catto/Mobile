7295cb52868b0a36d3e8b1320c8b10e80d0d8de6
Lucas Catto <lucascattosilva2018@gmail.com>
2023-08-23 10:50:13 -0300

    Mobile

---
+++ Login/Database/DDL.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE login_db;
+USE login_db;
+
+CREATE TABLE usuarios (
+id INT AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuarios (username, password) VALUES ('teste', 'senha123');
+
+SELECT * FROM usuarios;
+
